5e07be1cddbc7330c1f80f004f84394656e17473
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2024-01-26 11:29:11 -0500

    implemented basic add input to list function

---
+++ index.js
@@ -0,0 +1,19 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  //add elements to the output list
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  //render output
+  ulEl.innerHTML = listItems;
+}
