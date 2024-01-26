91199b397fe939e3e7ee246560d8d7c1919d36bf
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2024-01-26 17:30:37 -0500

    added delete all funtionalityupdated styling for delete button

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+//turn leads from string back to array to render
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
 
-inputBtn.addEventListener("click", function () {
-  // Check if the input value starts with "http://" or "https://"
-  let inputValue = inputEl.value
-  if (!inputValue.startsWith("http://") && !inputValue.startsWith("https://")) {
-    // Add "http://" prefix if missing
-    inputValue = "http://" + inputValue
-  }
-
-  myLeads.push(inputValue)
-  // Clear input field
-  inputEl.value = ""
-  //save myLeads into local storage
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  renderLeads()
-  console.log(localStorage.getItem("myLeads"))
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = ""
   //Add elements to the output list
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
       listItems += `
           <li>
-              <a target='_blank' href='${myLeads[i]}'>
-                  ${myLeads[i]}
+              <a target='_blank' href='${leads[i]}'>
+                  ${leads[i]}
               </a>
           </li>
       `
@@ -36,3 +26,28 @@ function renderLeads() {
   //Render output
   ulEl.innerHTML = listItems  
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+  //clear stoage
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+  // // Check if the input value starts with "http://" or "https://"
+  // let inputValue = inputEl.value
+  // if (!inputValue.startsWith("http://") && !inputValue.startsWith("https://")) {
+  //   // Add "http://" prefix if missing
+  //   inputValue = "http://" + inputValue
+  // }
+
+  myLeads.push(inputEl.value)
+  // Clear input field
+  inputEl.value = ""
+  //save myLeads into local storage, by convertion array to string
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+})
+
+
