5be7fb49ddf412fbf9f9ca347d4fdedc405b5f1e
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2024-01-26 17:03:53 -0500

    list is now correctly being added to the Chrome localstorage

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
 inputBtn.addEventListener("click", function () {
   // Check if the input value starts with "http://" or "https://"
   let inputValue = inputEl.value
@@ -14,21 +15,24 @@ inputBtn.addEventListener("click", function () {
   myLeads.push(inputValue)
   // Clear input field
   inputEl.value = ""
+  //save myLeads into local storage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
+  console.log(localStorage.getItem("myLeads"))
 });
 
 function renderLeads() {
   let listItems = ""
-  // Add elements to the output list
+  //Add elements to the output list
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-    <li>
-        <a target='_blank' href="${myLeads[i]}">
-            ${myLeads[i]}
-        </a>
-    </li>
-    `
+      listItems += `
+          <li>
+              <a target='_blank' href='${myLeads[i]}'>
+                  ${myLeads[i]}
+              </a>
+          </li>
+      `
   }
-  // Render output
-  ulEl.innerHTML = listItems;
+  //Render output
+  ulEl.innerHTML = listItems  
 }
